cfda0050e393054c0c139052d018a699e87f5b47
apioarpio <30659058+apioarpio@users.noreply.github.com>
2021-02-06 17:23:10 -0400

    primeiro commit

---
+++ src/components/basicos/ComParametro.jsx
@@ -0,0 +1,18 @@
+ import React from 'react'
+ 
+ export default function ComParametro(props) {
+     const status = props.nota >=7 ? 'Aprovado' : 'Recuperação'
+   
+     return (
+         <div>
+             <h1>{ props.titulo }</h1>
+             <p>
+                 <strong>{ props.aluno } </strong> 
+                 tem nota
+                 <strong> {props.nota } </strong>
+                 e está
+                 <strong> { status } </strong>!
+              </p>
+         </div>
+     )
+ }  
